e3cc8c084ad53b6218f2589cceb0000471a4061d
edwindev <erozog125@gmail.com>
2025-10-25 10:05:05 -0500

    Add menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import { Tasks } from "./components/Tasks/Tasks"
 import { Counter } from "./components/Counter/Counter"
 import { Background } from "./components/Background/Background"
+import { Menu } from "./components/Menu/Menu"
 
 export const App = () => {
   return (
     <div className="app">
+      <Menu />
       <Background />
       <Counter />      
+      <Tasks />
     </div>
   )
 }
---
+++ src/components/Menu/Menu.jsx
@@ -0,0 +1,14 @@
+import './Menu.css'
+
+export const Menu = () => {
+  return (
+    <nav>
+      <ul>
+        <li>Home</li>
+        <li>Counter</li>
+        <li>Background</li>
+        <li>Tasks</li>
+      </ul>
+    </nav>
+  )
+}
